5e36e13b95b963b9f82e0cd03fea879d38bb29f1
Paul Wright <31939113+SeburoSUMO@users.noreply.github.com>
2020-11-17 20:48:20 +0000

    Delete PicoScreenshot.sh

--- PicoScreenshot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-cd /home/USERNAME/Android/Sdk/platform-tools/ && ./adb kill-server && ./adb start-server && ./adb -d shell screencap -p /sdcard/filename.jpg && ./adb -d pull /sdcard/filename.jpg /home/USERNAME/Pictures/filename.jpg && ./adb -d shell rm /sdcard/filename.jpg
